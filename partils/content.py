a4053e2800068cfda65c2b20e7b28fd285ebccbd
Paulo <pauloqneto@gmail.com>
2025-05-19 20:25:55 -0300

    Experiências feitas

--- partils/content.py
+++ partils/content.py
@@ -2,8 +2,7 @@ import flet as ft
 
 class Banner(ft.Container):
     def __init__(self):
-        super().__init__(self)
-        self.expand = True
+        super().__init__()
         self.banner()
 
     def banner(self):         
@@ -13,8 +12,10 @@ class Banner(ft.Container):
             fit=ft.ImageFit.COVER,
             repeat=ft.ImageRepeat.NO_REPEAT,
             opacity=0.5,
+            scale=1,
         )
         self.content = ft.ResponsiveRow(
+            expand=True,
             columns=12,
             vertical_alignment=ft.CrossAxisAlignment.END,
             controls=[
@@ -56,8 +57,8 @@ class Banner(ft.Container):
                     col={"md":12, "lg":4},
                     content=ft.Image(
                         src="images/face-2.png",
-                        width=20,
-                        # scale=ft.Scale(scale=1.8, alignment=ft.alignment.bottom_center)
+                        fit=ft.ImageFit.COVER,
+                        repeat=ft.ImageRepeat.NO_REPEAT,
                     )
                 )
             ]
@@ -65,15 +66,103 @@ class Banner(ft.Container):
 
 class Experencia(ft.Container):
     def __init__(self):
-        super().__init__(self)
-        self.expand = True
+        super().__init__()
         self.conteudos()
 
     def conteudos(self):
-        self.content = ft.Row(
+        self.expand = True
+        self.content = ft.ResponsiveRow(
+            columns=12,
             controls=[
-                ft.Text(value="Experiência 1= aqui"),
-                ft.Text(value="Experiência 2= aqui")
+                ft.Text(
+                    col={"xs":6, "md":3},
+                    spans=[
+                        ft.TextSpan(
+                            text=" 1 + ",
+                            style=ft.TextStyle(
+                                color=ft.Colors.PRIMARY,
+                                weight=ft.FontWeight.W_900,
+                                size=20,
+                            )
+                        ),
+                        ft.TextSpan(
+                            text="Anos de experiência",
+                            style=ft.TextStyle(
+                                color=ft.Colors.WHITE,
+                                weight=ft.FontWeight.W_900,
+                                size=16,
+                            )
+                        )
+                    ]
+                ),
+
+
+                ft.Text(
+                    col={"xs":6, "md":3},
+                    spans=[
+                        ft.TextSpan(
+                            text=" 2 + ",
+                            style=ft.TextStyle(
+                                color=ft.Colors.PRIMARY,
+                                weight=ft.FontWeight.W_900,
+                                size=20,
+                            )
+                        ),
+                        ft.TextSpan(
+                            text="Linguagens de programação",
+                            style=ft.TextStyle(
+                                color=ft.Colors.WHITE,
+                                weight=ft.FontWeight.W_900,
+                                size=16,
+                            )
+                        )
+                    ]
+                ),
+
+
+                ft.Text(
+                    col={"xs":6, "md":3},
+                    spans=[
+                        ft.TextSpan(
+                            text=" 5 + ",
+                            style=ft.TextStyle(
+                                color=ft.Colors.PRIMARY,
+                                weight=ft.FontWeight.W_900,
+                                size=20,
+                            )
+                        ),
+                        ft.TextSpan(
+                            text="Projetos",
+                            style=ft.TextStyle(
+                                color=ft.Colors.WHITE,
+                                weight=ft.FontWeight.W_900,
+                                size=16,
+                            )
+                        )
+                    ]
+                ),
+
+                ft.Text(
+                    col={"xs":6, "md":3},
+                    spans=[
+                        ft.TextSpan(
+                            text=" 10k + ",
+                            style=ft.TextStyle(
+                                color=ft.Colors.PRIMARY,
+                                weight=ft.FontWeight.W_900,
+                                size=20,
+                            )
+                        ),
+                        ft.TextSpan(
+                            text="Corações felizes",
+                            style=ft.TextStyle(
+                                color=ft.Colors.WHITE,
+                                weight=ft.FontWeight.W_900,
+                                size=16,
+                            )
+                        )
+                    ]
+                )
             ],
             expand=True
         )
